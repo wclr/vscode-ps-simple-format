c5abc3898230939108e4e85f0fc3cca559318e36
wclr <alex.oshchepkov@gmail.com>
2021-09-04 14:15:42 +0500

    fix selection bugs

--- extension.ts
+++ extension.ts
@@ -7,15 +7,35 @@ import which from 'which'
 
 const { formatDefault, adjustIndentDefault } = require('purs-top-level-format')
 
-type Range = {
-  start: {
-    line: number
-    character: number
-  }
-  end: {
-    line: number
-    character: number
-  }
+type Formatter = {
+  name: string
+  bin: string
+  formatCmd: string | null
+  installInstruction: string
+}
+
+const formatters: { [N in string]: Formatter } = {
+  purty: {
+    name: 'purty',
+    bin: 'purty',
+    formatCmd: '-',
+    installInstruction:
+      'Do you have prettier and pose plugin installed? (`npm install --save-dev prettier @rowtype-yoga/prettier-plugin-purescript`)',
+  },
+  pose: {
+    name: 'pose',
+    bin: 'prettier',
+    formatCmd: '--stdin-filepath __dummy__format__.purs',
+    installInstruction:
+      'Do you have pose installed? (`npm install purty [-g]`)',
+  },
+  tidy: {
+    name: 'tidy',
+    bin: 'purs-tidy',
+    formatCmd: 'format',
+    installInstruction:
+      'Do you have purs-tidy installed? (`npm install purs-tidy [-g]`)',
+  },
 }
 
 const getDocRange = (document: vscode.TextDocument): vscode.Range => {
@@ -37,44 +57,36 @@ function activate(context: vscode.ExtensionContext): void {
     vscode.languages.registerDocumentFormattingEditProvider(
       { scheme: 'file', language: 'purescript' },
       {
-        provideDocumentFormattingEdits: (doc) =>
-          topFormat(doc, getDocRange(doc)),
+        provideDocumentFormattingEdits: (doc) => justFormat(doc),
       }
     ),
     vscode.languages.registerDocumentRangeFormattingEditProvider(
       { scheme: 'file', language: 'purescript' },
       {
         provideDocumentRangeFormattingEdits: (doc, range) =>
-          purtyFormat(doc, normalizeSelectedTextRange(doc, range)),
+          justFormat(doc, range),
       }
     )
   )
-}
-
-const topFormat = (
-  document: vscode.TextDocument,
-  range: vscode.Range
-): Promise<vscode.TextEdit[]> => {
-  output.appendLine('topFormat')
-  return Promise.resolve(formatDefault(document.getText(range)))
-    .then((stdout) => {
-      return [vscode.TextEdit.replace(range, stdout)]
-    })
-    .catch((err) => {
-      console.log(err)
-      return []
-    })
 }
 
 const rangeFromLines = (
   document: vscode.TextDocument,
   startLine: number,
   endLine: number
-) =>
-  new vscode.Range(
-    new vscode.Position(startLine, 0),
-    new vscode.Position(endLine, document.lineAt(endLine).text.length)
+) => {
+  const endLineReal = Math.max(0, Math.min(document.lineCount - 1, endLine))
+  const start = new vscode.Position(startLine, 0)
+  return new vscode.Range(
+    start,
+    startLine > endLine
+      ? start
+      : new vscode.Position(
+          endLineReal,
+          Math.max(0, document.lineAt(endLineReal).text.length)
+        )
   )
+}
 
 const normalizeSelectedTextRange = (
   document: vscode.TextDocument,
@@ -87,7 +99,8 @@ const getSymbolsCount = (text: string) => {
 
 const isWhiteSpace = (ch: string) => ch === ' '
 
-// positive count - remove (shift) from right to left <--
+// positive count: remove (shift) <-- from right to left (from the end)
+// negative count: remove (shift) --> from left to right (from the beginning)
 const removeSymbolsFromLine = (str: string, count: number) => {
   if (count === 0) return str
   const chars = str.split('')
@@ -96,6 +109,7 @@ const removeSymbolsFromLine = (str: string, count: number) => {
       ? chars.reduceRight(
           (p, ch) => {
             return {
+              // skip chars at the end
               res: p.count > 0 ? '' : ch + p.res,
               count: p.count - (isWhiteSpace(ch) ? 0 : 1),
             }
@@ -105,8 +119,9 @@ const removeSymbolsFromLine = (str: string, count: number) => {
       : chars.reduce(
           (p, ch) => {
             return {
-              res: p.count > 0 ? p.res + ch : '',
-              count: p.count - (isWhiteSpace(ch) ? 0 : 1),
+              // skip chars at the beginning
+              res: p.count <= 0 ? '' : p.res + ch,
+              count: p.count + (isWhiteSpace(ch) ? 0 : 1),
             }
           },
           { res: '', count }
@@ -122,15 +137,20 @@ const findLineSymCount = (
   start: { line: number; chars: number }
   end: { line: number; chars: number }
 } => {
-  const chars = text
-    .split('\n')
+  const lines = text.split('\n')
+
+  const chars = lines
     .map((s) => s.trim())
     .join('\n')
     .split('')
   const isNewLine = (ch: string) => ch === '\n'
   const getLine = (dir: 1 | -1, count: number) => {
     const method = dir === 1 ? 'reduce' : 'reduceRight'
+
+    const [startLine] = dir === 1 ? [0, 0] : [lines.length - 1]
+
     output.appendLine(['getLine', count, dir].join(' '))
+
     const r = chars[method](
       (p, ch) => {
         const isNew = isNewLine(ch)
@@ -150,7 +170,7 @@ const findLineSymCount = (
               line: p.line + (isNew ? dir : 0),
             }
       },
-      { count: 0, line: 0, chars: 0 }
+      { count: 0, line: startLine, chars: 0 }
     )
     return {
       chars: r.chars,
@@ -168,52 +188,140 @@ const removeCharsAndSplit = (text: string, charsToRemove: number) => {
   return removeSymbolsFromLine(text, charsToRemove).split('/n')
 }
 
-const purtyFormat = (
+const debugOutput = (str: string) => output.appendLine(str)
+
+const justFormat = (
   document: vscode.TextDocument,
-  range: vscode.Range
+  selRange?: vscode.Range
 ): Promise<vscode.TextEdit[]> => {
-  //console.debug('formatting purescript')
-  output.appendLine('Getting top range')
+  const configuration = vscode.workspace.getConfiguration()
+
+  const config = {
+    ...defaultConfig,
+    ...configuration.get<Config>('purescript.topLevelFormat'),
+  }
+
+  debugOutput('config: ' + JSON.stringify(config))
+
+  const formatterName = config.formatter
+  const formatter =
+    config.customFormatter ||
+    (formatterName ? formatters[formatterName] : false)
+
+  if (!formatter || !selRange) {
+    const text = document.getText()
+    if (formatter && !config.onlySelection) {
+      return runFormatter(formatter, text, document.fileName).then(
+        (formatted) => {
+          return [
+            vscode.TextEdit.replace(
+              getDocRange(document),
+              formatDefault(formatted)
+            ),
+          ]
+        }
+      )
+    } else {
+      debugOutput('Formatting whole document')
+      return Promise.resolve([
+        vscode.TextEdit.replace(getDocRange(document), formatDefault(text)),
+      ])
+    }
+  }
+
+  debugOutput('Getting top range')
+
+  const range = normalizeSelectedTextRange(document, selRange)
+
+  debugOutput('range')
   const topRange = getTopLevelRange(document, range)
-  // vscode.window.showInformationMessage(
-  //   ['Range', topRange.start.line, topRange.end.line].join(':')
-  // )
-  output.appendLine(
-    ['Top range (lines)', topRange.start.line, topRange.end.line].join(':')
+
+  debugOutput(
+    [
+      'Top range (lines)',
+      [topRange.start.line, topRange.start.character].join(':'),
+      [topRange.end.line, topRange.end.character].join(':'),
+    ].join(' ')
   )
 
-  // const selectedText = document.getText(range)
-  // const wholeTopBlockText = document.getText(topRange)
-
-  const textFromStart = document.getText(
+  // lines before selected
+  const textBeforeSelected = document.getText(
     rangeFromLines(document, topRange.start.line, range.start.line - 1)
   )
 
-  const textToEnd = document.getText(
-    rangeFromLines(document, range.end.line + 1, topRange.end.line)
+  // do not include last line of the selection if not chars selected there
+  const rangeEndLine =
+    range.end.character === 0 ? Math.max(0, range.end.line - 1) : range.end.line
+
+  debugOutput('rangeEndLine: ' + rangeEndLine)
+
+  const textAfterSelected = document.getText(
+    rangeFromLines(document, rangeEndLine + 1, topRange.end.line)
   )
 
-  return purty(document, topRange)
-    .then((formatted) => {
+  const [text, revert] = geTextToFormat(document.getText(topRange))
+
+  // format a whole top level blocks referred to selection
+  return runFormatter(formatter, text, document.fileName)
+    .then((formattedRes) => {
+      const formatted = revert(formattedRes)
+
+      output.appendLine('formatted:\n' + formatted)
+
       const symLines = findLineSymCount(
         formatted,
-        getSymbolsCount(textFromStart),
-        getSymbolsCount(textToEnd)
+        getSymbolsCount(textBeforeSelected),
+        getSymbolsCount(textAfterSelected)
       )
+
       output.appendLine('symLines:' + JSON.stringify(symLines))
+      output.appendLine('formatted lines: ' + formatted.split('\n').length)
+      // extract from the formatted piece
+      // lines that will be replaced in the code
       const formattedCutLines = formatted
         .split('\n')
-        .slice(symLines.start.line, symLines.end.line)
+        .slice(symLines.start.line, symLines.end.line + 1)
 
-      const topWithFormattedCut = [
-        ...removeCharsAndSplit(textFromStart, symLines.start.chars),
-        ...formattedCutLines,
-        removeCharsAndSplit(textToEnd, symLines.end.chars),
-      ].join('\n')
+      const beforeLines =
+        textBeforeSelected.length > 1
+          ? removeCharsAndSplit(textBeforeSelected, symLines.start.chars)
+          : []
+      output.appendLine('beforeLines: ' + beforeLines.length)
 
+      output.appendLine(
+        'textBeforeSelected.length: ' + textBeforeSelected.length
+      )
+
+      output.appendLine(
+        `textBeforeSelected(${getSymbolsCount(textBeforeSelected)}):\n` +
+          textBeforeSelected
+      )
+      output.appendLine(
+        `textAfterSelected(${getSymbolsCount(textAfterSelected)}):\n` +
+          textAfterSelected
+      )
+
+      const afterLines = textAfterSelected
+        ? removeCharsAndSplit(textAfterSelected, symLines.end.chars)
+        : []
+
+      output.appendLine('afterLines: ' + afterLines.length)
+      output.appendLine('afterLines text: ' + afterLines)
+
+      output.appendLine('formattedCut lines: ' + formattedCutLines.length)
       output.appendLine('formattedCut:\n' + formattedCutLines.join('\n'))
 
-      output.appendLine('whole:\n' + topWithFormattedCut)
+      const topWithFormattedCut = [
+        ...beforeLines,
+        ...formattedCutLines,
+        ...afterLines,
+      ].join('\n')
+
+      output.appendLine(
+        'topWithFormattedCut lines: ' + topWithFormattedCut.split('\n').length
+      )
+      output.appendLine('topWithFormattedCut:\n' + topWithFormattedCut)
+      output.appendLine('<-- topWithFormattedCut end.')
 
       // output.appendLine('formatted:' + formatted)
       const whole = [
@@ -246,7 +354,7 @@ const geTextToFormat = (text: string): [string, (text: string) => string] => {
   const str = 'module X where\n'
   if (!/^\s*module\s*([\S]*)/.test(text)) {
     const emptySpace = text.match(/\s*/)![0]
-    return [str + text, (res) => emptySpace + res.substr(str.length + 1)]
+    return [str + text, (res) => emptySpace + res.substr(str.length)]
   } else {
     return [text, (res) => res]
   }
@@ -254,6 +362,7 @@ const geTextToFormat = (text: string): [string, (text: string) => string] => {
 
 type Dir = -1 | 1
 
+// gets whole top block(s) selection, which to actually selected range refer
 const getTopLevelRange = (
   document: vscode.TextDocument,
   range: vscode.Range
@@ -268,15 +377,17 @@ const getTopLevelRange = (
   const lineCount = document.lineCount
   output.appendLine('lineCount:' + lineCount)
 
+  // finds top line declaration moves in dir - up (-1), down (1)
   const findTop = (
     index: number,
     dir: Dir,
     allowOnlyEmpty: boolean
   ): number => {
-    if (index >= lineCount - 1) return lineCount - 1
+    // we may return illegal lineCount number, but it is ok
+    if (index > lineCount - 1) return lineCount
 
     const text = document.lineAt(index).text.trimRight()
-    output.appendLine(['findTop', allowOnlyEmpty, dir, index, text].join(':'))
+    // output.appendLine(['findTop', allowOnlyEmpty, dir, index, text].join(':'))
     const isEmpty = text === ''
     const isNonTop = text.startsWith(' ')
 
@@ -289,7 +400,9 @@ const getTopLevelRange = (
 
   // find prev(dir: -1)/next(dir: 1) top level string
   const findTopBiDir = (startIndex: number, dir: Dir) => {
-    output.appendLine(['findTopBiDir startIndex', startIndex, dir].join(':'))
+    output.appendLine(
+      ['findTopBiDir startIndex:', startIndex, ' dir:', dir].join(' ')
+    )
     // first we try to look up for top level in opposite direction
     const rev = findTop(startIndex, -dir as Dir, true)
     return rev >= 0 ? rev : findTop(startIndex, dir, false)
@@ -308,48 +421,67 @@ const getTopLevelRange = (
   return rangeFromLines(document, startTopLine, endOfBlock)
 }
 
-const purty = async (
-  document: vscode.TextDocument,
-  range: vscode.Range
+type Config = {
+  formatter?: 'purty' | 'tidy' | 'pose'
+  customFormatter?: Formatter
+  onlySelection?: boolean
+}
+
+const defaultFormatter = 'purty'
+
+const defaultConfig: Config = {
+  formatter: defaultFormatter,
+  onlySelection: true,
+}
+
+const runFormatter = async (
+  formatter: Formatter,
+  text: string,
+  filePath: string
 ): Promise<string> => {
-  const configs = vscode.workspace.getConfiguration('purty')
+  output.appendLine('Using formatter bin: ' + formatter.bin)
+
   // We use empty string to mean unspecified because it means that the setting
   // can be edited without having to write json (`["string", "null"]` does not
   // have this property).
-  const purtyCmd = await findPurty(document.fileName, configs.pathToPurty)
-  if (purtyCmd == null) {
+  const formatterCmd = await findFormatter(formatter, filePath, formatter.bin)
+
+  if (formatterCmd == null) {
     vscode.window.showErrorMessage(
-      `Error: Could not find location of purty exe.`
+      `Error: Could not find location of formatter binary: ${formatter.bin}`
     )
-    vscode.window.showInformationMessage(
-      'Do you have purty installed? (`npm install purty`)'
-    )
-    vscode.window.showInformationMessage(
-      'Or you can specify the full path in settings.'
-    )
-    return Promise.reject('cannot find purty exe')
+    vscode.window.showInformationMessage(formatter.installInstruction)
+
+    return Promise.reject(`cannot find ${formatter.bin} binary`)
   }
+
+  const cmdAdd = formatter.formatCmd ? ' ' + formatter.formatCmd : ''
   // Quotes make sure any strange characters in the path are escaped (single quotes would
   // be better, but double quotes are more cross-platform (works on windows))
-  const cmd = `"${purtyCmd}" -`
-  const [text, revert] = geTextToFormat(document.getText(range))
+  const cmd = `"${formatterCmd}"` + cmdAdd
+
   const cwdCurrent = vscode.workspace.rootPath
   return new Promise((resolve, reject) => {
+    output.appendLine('running: ' + cmd)
     const childProcess = exec(
       cmd,
       { cwd: cwdCurrent },
       (err, stdout, stderr) => {
         if (stderr) {
           vscode.window.showErrorMessage(
-            'Could not format with purty:' + stderr // + '\nInput' + text
+            `Could not format using ${formatter.name} formatter:` + stderr // + '\nInput' + text
           )
         }
         if (err || stderr) {
           reject(err || stderr)
         }
-        resolve(revert(stdout))
+        resolve(stdout)
       }
     )
+    childProcess.on('error', (err) => {
+      output.appendLine('childProcess error: ' + err.message)
+    })
+    output.appendLine('writing text\n' + text)
     childProcess.stdin!.write(text)
     childProcess.stdin!.end()
   })
@@ -361,18 +493,22 @@ const purty = async (
 /// then/otherwise the location (<workspace_dir>/node_modules/.bin/purty) is searched,
 /// and finally the `PATH` environment variable used for a final search. If no executable
 /// is found, then the promise will be rejected.
-const findPurty = async (psFilePath: string, purtyPath: string) => {
+const findFormatter = async (
+  formatter: Formatter,
+  psFilePath: string,
+  purtyPath: string
+) => {
   if (purtyPath !== '') {
     if (await canRun(purtyPath)) {
       return purtyPath
     }
   }
-  const localPurty = await localPurtyPath(psFilePath)
+  const localPurty = await localFormatterPath(formatter.bin, psFilePath)
   if (await canRun(localPurty)) {
     return localPurty
   }
   try {
-    return await which('purty')
+    return await which(formatter.bin)
   } catch (_) {}
   return null
 }
@@ -391,7 +527,7 @@ const canRun = async (exePath: string | null) => {
 
 /// Get the location that `npm install purty` would install purty to. If no file
 /// exists there or it is not executable, return `null`.
-const localPurtyPath = async (cwd: string) => {
+const localFormatterPath = async (binName: string, cwd: string) => {
   try {
     const workspacePath = await pkgUp({ cwd })
     if (!workspacePath) {
@@ -401,7 +537,7 @@ const localPurtyPath = async (cwd: string) => {
       path.dirname(workspacePath),
       'node_modules',
       '.bin',
-      'purty'
+      binName
     )
     if (await canRun(purtyPath)) {
       return purtyPath
